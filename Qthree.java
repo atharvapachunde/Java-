6006a440e3f40a03183f3860cebeeaf53e805a64
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2024-08-09 22:47:09 +0530

    Add files via upload

---
+++ Qthree.java
@@ -0,0 +1,16 @@
+// take two variables 'a' and 'b' and print their sum.
+import java.util.*;
+public class Qthree
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        int result = a + b;
+        System.out.println("Result is " + result);
+
+    }
+}
