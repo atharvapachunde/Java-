9513e71b8c1209cf2c59ae18f06edf64f478150c
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2024-08-11 22:42:01 +0530

    Add files via upload

---
+++ Evod.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class Evod
+{
+    public static void main(String args[])
+    {
+        int x;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number    ");
+        x = sc.nextInt();
+        if (x%2 == 0)
+            System.out.println("Given number is Even");
+        else 
+            System.out.println("Given number is Odd");            
+
+    }
+}
